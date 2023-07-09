51f9b21134c0f11df14f84ea14968fe27b2fe31f
Mohamed Hassanin <hassanin@udel.edu>
2023-07-08 21:19:18 -0400

    add1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use crate::my_cli::foo::say_hello;
-use azure_core::auth::TokenCredential; 
-use azure_identity::{DefaultAzureCredential};
+use azure_core::auth::TokenCredential;
+use clap::Parser;
+mod azure;
+use azure_identity::DefaultAzureCredential;
 use url::Url;
 // use my_cli::hello;
 mod my_cli;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     my_cli::hello();
     say_hello();
     my_cli::foo::say_hello();
+    let args: Vec<String> = std::env::args().collect();
+    println!("args: {:?}", args);
+    let args2 = crate::my_cli::Cli::parse();
+    println!("args2: {:?}", args2);
+    azure::do_stuff().await?;
     Ok(())
 }
--- src/my_cli/mod.rs
+++ src/my_cli/mod.rs
@@ -1,6 +1,27 @@
-//  mod my_cli;
+// use clap::Parser;
+use std::ffi::OsStr;
+use std::ffi::OsString;
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand, ValueEnum};
 pub mod foo;
-// Path: src/my_cli.rs
 pub fn hello() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Parser)]
+pub struct Cli{
+    #[clap(subcommand)]
+    command:Commands
+}
+#[derive(Debug, Subcommand)]
+enum Commands{
+  Free(FreeQuestion)
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+struct FreeQuestion{
+    #[arg(short, long)]
+    question: String,
 }
